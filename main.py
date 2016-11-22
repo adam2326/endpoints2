15e38180cbe7bab53b5c8299ca34ba1fee37b2be
adam2326 <adampilz.2326@gmail.com>
2016-11-22 16:34:59 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import random
+import string
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index_page():
+  word = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(5)])
+	return '<html><body>{}</body></html>'.format(word)
