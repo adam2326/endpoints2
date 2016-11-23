b28d8f61d09ef46ef6a90c77657d6992771cd09c
adam2326 <adampilz.2326@gmail.com>
2016-11-23 12:04:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import random
 import string
 
@@ -8,4 +8,5 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index_page():
   word = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(5)])
-  return '<html><body>{}</body></html>'.format(word)
+  json_resp = jsonify(target_api='word_converter', word=word)
+  return '<html><body>{}</body></html>'.format(json_response)
